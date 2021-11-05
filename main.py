78eadc2e89fdef2b3ed70edbfeb6d092c62ed859
Biton's Mac <idanbit80@gmail.com>
2021-11-05 16:15:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import os
+
+## Ready or Heroku deploy.
+
+chrome_options = webdriver.ChromeOptions()
+# options.add_argument("--window-size=1100,900")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# Now you can start using Selenium
+browser.get("https://topirzul.co.il/admin-kivi")
+print(browser.title)
