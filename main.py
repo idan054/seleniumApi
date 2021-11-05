d486281ef3eb961a4f6a6b18a410274941f9e523
Biton's Mac <idanbit80@gmail.com>
2021-11-05 16:46:32 +0200

    a

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(executable_pathheroku=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Now you can start using Selenium
 browser.get("https://topirzul.co.il/admin-kivi")
